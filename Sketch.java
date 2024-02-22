10d962b40968b1daf7f56b408f29b5fde0c6f221
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-02-22 13:16:40 -0500

    I'm done

--- Sketch.java
+++ Sketch.java
@@ -1,5 +1,8 @@
 import processing.core.PApplet;
-
+  /**
+  * A program that draws a human face that can adapt to any window size
+  * @author: A. Razack
+  */
 public class Sketch extends PApplet {
 	
 	
@@ -7,8 +10,9 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
+  
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,21 +20,33 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // Draw head
+    fill(255, 204, 153); // Set fill color to light orange
+    ellipse((float) (width/2), (float) (height/2), (float) (width/2), (float) (height/2)); // Draw ellipse for head
   
-  // define other methods down here.
+    // Draw eyes
+    fill(255); // Set fill color to white
+    ellipse((float) (width/2.35294117647), (float) (height/2.35294117647), (float) (width/10), (float) (height/10)); // Draw left eye
+    ellipse((float) (width/1.73913043478), (float) (height/2.35294117647), (float) (width/10), (float) (height/10)); // Draw right eye
+  
+    // Draw pupils
+    fill(0); // Set fill color to black
+    ellipse((float) (width/2.35294117647), (float) (height/2.35294117647), (float) (width/20), (float) (height/20)); // Draw left pupil
+    ellipse((float) (width/1.73913043478), (float) (height/2.35294117647), (float) (width/20), (float) (height/20)); // Draw right pupil
+  
+    // Draw nose
+    fill(255, 102, 102); // Set fill color to pink
+    triangle((float) (width/2), (float) (height/2.10526315789), (float) (width/2.10526315789), (float) (height/1.81818181818), (float) (width/1.90476190476), (float) (height/1.81818181818)); // Draw nose
+  
+    // Draw mouth
+    arc((float) (width/2), (float) (height/1.66666666667), (float) (width/4), (float) (height/5), 0, PI); // Draw mouth arc
+  }
 }
